087fcc21c327980a1c28abf3c2f321f6dc01305b
wang-y-z <57429717+wang-y-z@users.noreply.github.com>
2020-03-21 12:29:00 +0800

    finish most of all

--- 0321node/02todolistAPI/server.js
+++ 0321node/02todolistAPI/server.js
@@ -6,8 +6,21 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var routes = require('./app/routes/todoListRoutes'); //importing route
+var routes = require('./app/todoListRoutes/todoListRoutes'); //importing route
 routes(app); //register the route
 app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
+
+
+/*var http = require('http');
+
+http.createServer(function (request, response) {
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');*/
